b4be0d39898abf734c291e803264d1d8e845edd0
crist <crist@LAPTOP-S85655LI>
2022-09-19 22:25:53 -0500

    Agregar archivos de proyecto.

---
+++ API-invvideojuegos/Controllers/videojuegosController.cs
@@ -0,0 +1,22 @@
+﻿using API_invvideojuegos;
+using API_invvideojuegos.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_invvideojuegos.Controllers
+{
+    [ApiController]
+    [Route("/videojuegos")]
+    public class videojuegosController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<videojuegos>> Get()
+        {
+            return new List<videojuegos>()
+          {
+              new videojuegos(){ Id = 1, Name = "halo"  },
+              new videojuegos(){ Id = 1, Name = "minecraft"}
+          };
+
+        }
+    }
+}
---
+++ API-invvideojuegos/Entidades/videojuegos.cs
@@ -0,0 +1,8 @@
+﻿namespace API_invvideojuegos.Entidades
+{
+    public class videojuegos
+    {
+        public int Id { get; set; }
+        public string Name { get; internal set; }
+    }
+}
